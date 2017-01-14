b9eca5907d761c222ba1e197c328994938be2722
Robbie Sherman <rob.w.sherman@gmail.com>
2017-01-14 15:25:19 +0000

    add get todo/id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyparser = require('body-parser');
+const {ObjectID} = require('mongodb');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -30,8 +31,25 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos/:id', (req, res) => {
+	let id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		return res.status(404).send();
+	}
+
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			res.status(404).send();
+		}
+		res.send({todo});
+	}).catch((e) => {
+		res.status(400).send();
+	});
+});
+
 app.listen(3000, () => {
 	console.log('App started on port 3000');
 });
 
-module.exports= {app};
+module.exports = {app};
