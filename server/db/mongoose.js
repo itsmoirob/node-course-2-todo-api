2df3dbf4af45f1f74c29c53307bd239e706c8cd3
Robbie Sherman <rob.w.sherman@gmail.com>
2017-01-18 18:34:51 +0000

    add db variable

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
+let db = {
+	localhost: 'mongodb://localhost:27017/TodoApp',
+	mLab: 'mongodb://dev:d382ec9aw47PZUPL@ds117109.mlab.com:17109/node-2-todo-api'
+};
+
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://dev:d382ec9aw47PZUPL@ds117109.mlab.com:17109/node-2-todo-api');
+mongoose.connect( db.localhost || db.mlab);
 
 
 module.exports = {
