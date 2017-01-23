9352539f9a8ec45720abe96a2094966f1fd989fa
Robbie Sherman <rob.w.sherman@gmail.com>
2017-01-23 18:40:46 +0000

    setup seperate test and dev envs

---
+++ server/config/config.js
@@ -0,0 +1,11 @@
+var env = process.env.NODE_ENV || 'development' ;
+
+if(process.env.NODE_ENV === 'production') {
+	process.env.MONGODB_URI = 'mongodb://dev:d382ec9aw47PZUPL@ds117109.mlab.com:17109/node-2-todo-api';
+} else if(env === 'development') {
+	process.env.PORT = 3000;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoApp';
+} else if(env === 'test') {
+	process.env.PORT = 3000;
+	process.env.MONGODB_URI = 'mongodb://localhost:27017/TodoAppTest';
+}
--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,7 @@
 const mongoose = require('mongoose');
 
-let db = {
-	localhost: 'mongodb://localhost:27017/TodoApp',
-	mLab: 'mongodb://dev:d382ec9aw47PZUPL@ds117109.mlab.com:17109/node-2-todo-api'
-};
-
 mongoose.Promise = global.Promise;
-mongoose.connect( db.localhost, () => {
+mongoose.connect(process.env.MONGODB_URI, () => {
 	console.log('Connected to mongodb');
 });
 
